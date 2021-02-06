1305f407b2c768cd8bfb03dbb056a88c2a25d298
DeepankurK <deepank@iitk.ac.in>
2021-02-06 22:29:26 +0530

    reader

--- src/scanner.c
+++ src/scanner.c
@@ -1,1 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+    FILE *fh;
+	FILE *fo;
+    extern FILE *yyin;
+    extern FILE *yyout;
+    if (argc == 2 && (fh = fopen(argv[1], "r")))
+        yyin = fh;
+	if (argc == 3 && (fo= fopen(argv[2], "w+")))
+        yyout = fo;
+	while(1)
+	{yylex();
+	}
+    return 0;
+}  
