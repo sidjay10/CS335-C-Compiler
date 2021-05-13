32aa189ab52cd2478dcf8fa79623a25158b5c8fa
Yugesh Kothari <yugesh.kothari@gmail.com>
2021-05-13 15:42:27 +0530

    Malloc asm

--- lib/lib.asm
+++ lib/lib.asm
@@ -1,41 +1,54 @@
-	.text
-	.globl print_int
+.text
+.globl print_int
 print_int:
 	li $v0, 1
 	syscall
 	jr $ra
 	
-	.text
-	.globl scan_int 
+.text
+.globl scan_int 
 scan_int:
 	li $v0, 5
 	syscall
 	jr $ra
 
-	.text
-	.globl print_char
+.text
+.globl print_char
 print_char:
 	li $v0, 11
 	syscall
 	jr $ra
 
-	.text
-	.globl scan_char
+.text
+.globl scan_char
 scan_char:
 	li $v0, 12
 	syscall
 	jr $ra
 	
-	.text
-	.globl print_string
+.text
+.globl print_string
 print_string:
 	li $v0, 4
 	syscall
 	jr $ra
 	
-	.text
-	.globl scan_string
+.text
+.globl scan_string
 scan_string:
 	li $v0, 8
+	syscall
+	jr $ra
+
+
+# Function : malloc
+# Signature : void* malloc(int n)
+# Returns : pointer to allocated memory
+
+.text
+.globl malloc
+malloc:
+	# Takes one arguement $a0 to indicate number of bytes
+	li $v0, 9
 	syscall
 	jr $ra
